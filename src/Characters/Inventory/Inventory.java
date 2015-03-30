450a6f0f43511f00f30216585d38e7e150b8aa7f
milessanguinetti <embakaokami@gmail.com/em>
2015-03-29 21:07:53 -0700

    added item types and inventory implementation

--- src/Characters/Inventory/Inventory.java
+++ src/Characters/Inventory/Inventory.java
@@ -1,5 +1,7 @@
 package Characters.Inventory;
 
+import Structures.Node;
+import Structures.splayNode;
 import Structures.splayTree;
 
 import java.util.Scanner;
@@ -25,8 +27,89 @@ public class Inventory {
         if(input < 1 || input > 5)
             return; //escape input
         splayTree selected = Items[input - 1]; //select a tree
-        
+        selected.Display();
+        if(input == 1)
+            System.out.println("Would you like to use an item?");
+        else if(input < 5 && input > 1)
+            System.out.println("Would you like to equip an item?");
+        else{
+            System.out.println("Enter '0' to exit or '1' to view another category.");
+            input = scanner.nextInt();
+            scanner.nextLine();
+            if(input == 1){
+                Interface(); //recursive call
+            }
+            return;
+        }
+        System.out.println("Enter '0' to exit, '1' to select an item, or '2' to view another category.");
+        input = scanner.nextInt();
+        scanner.nextLine();
+        if(input == 0){
+            return;
+        }
+        else if(input == 1){
+            System.out.println("Enter the item's name or enter '0' to cancel.");
+            String strInput = scanner.nextLine();
+            if(!strInput.equals("0")){ //only do anything if the user didn't enter 0
+                Item Retrieved = ((Item)selected.Retrieve(strInput).returnData());
+                if(Retrieved.compareTo(strInput) == 0)
+                    Retrieved.Use();
+                else{
+                    System.out.println("Item not found. Closest match is:");
+                    Retrieved.Display();
+                    System.out.println("Use this item? Enter '0' for no or '1' for yes.");
+                    input = scanner.nextInt();
+                    scanner.nextLine();
+                    if(input == 1)
+                        Retrieved.Use();
+                }
+            }
+            System.out.println("Would you like to do more with your inventory?");
+            System.out.println("Enter '0' for no or '1' for yes.");
+            input = scanner.nextInt() + 1; //add 1 to match with previous other category call
+            scanner.nextLine();
+        }
+        if(input == 2)
+            Interface();
     }
 
+    //selects an item during combat by skipping to the consumables splay tree
+    public Consumable combatInterface() {
+        Scanner scanner = new Scanner(System.in);
+        int input;
+        System.out.println("Enter the item's name or enter '0' to cancel.");
+        String strInput = scanner.nextLine();
+        if (!strInput.equals("0")) { //only do anything if the user didn't enter 0
+            Item Retrieved = ((Item) Items[0].Retrieve(strInput).returnData());
+            if (Retrieved.compareTo(strInput) == 0)
+                return ((Consumable)Retrieved);
+            else {
+                System.out.println("Item not found. Closest match is:");
+                Retrieved.Display();
+                System.out.println("Use this item? Enter '0' for no or '1' for yes.");
+                input = scanner.nextInt();
+                scanner.nextLine();
+                if (input == 1)
+                    return ((Consumable)Retrieved);
+            }
+        }
+        return null; //if they didn't want to use an item after all, return null.
+    }
 
+    public void Insert(Item toInsert){
+        splayTree itemType = null;
+        if(toInsert instanceof Consumable)
+            itemType = Items[0];
+        else if(toInsert instanceof Weapon)
+            itemType = Items[1];
+        else if(toInsert instanceof Armor)
+            itemType = Items[2];
+        else if(toInsert instanceof Accessory)
+            itemType = Items[3];
+        else if(toInsert instanceof questItem)
+            itemType = Items[4];
+        if(itemType == null) //if this isn't actually an item, we can't insert it safely.
+            return;
+        itemType.Insert(new splayNode(toInsert));
+    }
 }
