c9ece4b714c2d70cc490ee54fcfb39391a181f03
milessanguinetti <bakaokami@gmail.com>
2015-06-21 20:58:44 -0700

    map work, map work, map work

--- src/Maps/Room.java
+++ src/Maps/Room.java
@@ -3,27 +3,50 @@ package Maps;
 /**
  * Created by Miles Sanguinetti on 5/20/2015.
  */
-public abstract class Room {
-
+public class Room {
+    private static String mapName; //the name of the current map; having a static variable
     private int [] Tiles = new int[81];
     /*
     Each normal room contains 9 * 9 tiles; this is not a multidimensional array, but to the end of saving memory,
-    but is rather a single lengthy block of contiguous integers. To go up or down, one would simply add or subtract 9.
-    9x9 tiles denotes 91 spaces.
+    is rather a single lengthy block of contiguous integers. To go up or down, one would simply add or subtract 9.
+    9x9 tiles denotes 769 spaces.
     */
     private int x; //player's x and y coordinates
     private int y;
 
     Room(){
-        for(int i = 0; i < 81; ++i){
 
-        }
     }
 
-    public abstract void Draw();
+    public void setMapName(String toName){
+        mapName = toName;
+    }
 
-    public abstract void Enter(int Direction); //enter the room from a cardinal direction
+    public void Draw(){
 
-    public abstract boolean moveTo(int x, int y); //returns false if the desired coordinate is innavigable
-    
+    }
+
+    public void Enter(int Direction) { //enter the room from a cardinal direction
+
+    }
+
+    public boolean moveTo(int x, int y) { //returns false if the desired coordinate is innavigable
+
+        return true;
+    }
+
+    public void generateTiles(int roomType){ //generates tiles according to a passed room format:
+        /*
+        1 = ^
+        2 = >
+        3 = \/
+        4 = <
+        5 = ^ \/
+        6 = < >
+        7 = < ^ >
+        8 = < \/ >
+        9 = < ^ > \/
+         */
+
+    }
 }
--- src/Maps/Tileset.java
+++ src/Maps/Tileset.java
@@ -1,7 +1,12 @@
 package Maps;
 
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Random;
 
 /**
@@ -21,23 +26,75 @@ public class Tileset {
      */
 
     ImageView getExit(int Direction){
+        try(InputStream imginput = Files.newInputStream(Paths.get(Name + "exit.png"))){
+            ImageView toReturn = new ImageView(new Image(imginput));
+            toReturn.setFitWidth(96);
+            toReturn.setFitHeight(96); //preserve aspect ratio
+            return toReturn;
+        }
 
+        catch (IOException e){
+            System.out.println("Error loading image.");
+            return null;
+        }
     }
 
     ImageView getWall(int Direction){
+        int which = Rand.nextInt(3) + 1;
+        try(InputStream imginput = Files.newInputStream(Paths.get(Name + "wall" + which + ".png"))){
+            ImageView toReturn = new ImageView(new Image(imginput));
+            toReturn.setFitWidth(96);
+            toReturn.setFitHeight(96); //preserve aspect ratio
+            return toReturn;
+        }
 
+        catch (IOException e){
+            System.out.println("Error loading image.");
+            return null;
+        }
     }
 
     ImageView getCorner(int Direction){
+        try(InputStream imginput = Files.newInputStream(Paths.get(Name + "corner.png"))){
+            ImageView toReturn = new ImageView(new Image(imginput));
+            toReturn.setFitWidth(96);
+            toReturn.setFitHeight(96); //preserve aspect ratio
+            return toReturn;
+        }
 
+        catch (IOException e){
+            System.out.println("Error loading image.");
+            return null;
+        }
     }
 
     ImageView getGround(){
+        int which = Rand.nextInt(6) + 1;
+        try(InputStream imginput = Files.newInputStream(Paths.get(Name + "ground" + which + ".png"))){
+            ImageView toReturn = new ImageView(new Image(imginput));
+            toReturn.setFitWidth(96);
+            toReturn.setFitHeight(96); //preserve aspect ratio
+            return toReturn;
+        }
 
+        catch (IOException e){
+            System.out.println("Error loading image.");
+            return null;
+        }
     }
 
     ImageView getTreasure(){
+        try(InputStream imginput = Files.newInputStream(Paths.get(Name + "treasure.png"))){
+            ImageView toReturn = new ImageView(new Image(imginput));
+            toReturn.setFitWidth(96);
+            toReturn.setFitHeight(96); //preserve aspect ratio
+            return toReturn;
+        }
 
+        catch (IOException e){
+            System.out.println("Error loading image.");
+            return null;
+        }
     }
 
     ImageView getObstacle(int Which){
@@ -46,6 +103,36 @@ public class Tileset {
         2 = 2x2
         3 = 3x2
          */
+        String addendum = null;
+        if(Which == 1){
+            addendum = "obst1x2.png";
+        }
+        else if(Which == 2){
+            int roll = Rand.nextInt(2);
+            if(roll == 0)
+                addendum = "obst2x2.png";
+            else
+                addendum = "obst2x2(2).png";
+        }
+        else if(Which == 3){
+            addendum = "obst3x2.png";
+        }
+
+        if(addendum == null){
+            return null;
+        }
+
+        try(InputStream imginput = Files.newInputStream(Paths.get(Name + addendum))){
+            ImageView toReturn = new ImageView(new Image(imginput));
+            toReturn.setFitWidth(96);
+            toReturn.setFitHeight(96); //preserve aspect ratio
+            return toReturn;
+        }
+
+        catch (IOException e){
+            System.out.println("Error loading image.");
+            return null;
+        }
 
     }
 }
